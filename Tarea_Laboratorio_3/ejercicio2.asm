cfc96809d3f3fef88804d259456c30d1ea35bac5
unknown <00090718@uca.edu.sv>
2021-05-10 01:19:47 -0600

    Ejercicio2: fixed

--- Tarea_Laboratorio_3/ejercicio2.asm
+++ Tarea_Laboratorio_3/ejercicio2.asm
@@ -9,7 +9,7 @@ section .text
                 CMP CL, 00
                 JZ salto 
 
-condic      MUL CX 
+condic          MUL CX 
                 LOOP condic 
-salto       MOV [20BH], AL
+salto           MOV [20BH], AL
                 int 20h
