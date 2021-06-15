de677d444069971087d807a1dbb9289c1bdebe5c
unknown <00090718@uca.edu.sv>
2021-06-15 01:12:28 -0600

    Labo5: fixing code

--- Tarea_Laboratorio_5/ejercicio1.asm
+++ Tarea_Laboratorio_5/ejercicio1.asm
@@ -1,79 +1,79 @@
 ;Codigo base bioscadena
 org 100h
         section .text
-        xor AX, AX
-        xor SI, SI
-        xor BX, BX
-        xor CX, CX
-        xor DX, DX
 
         MOV SI, 0
         MOV DI, 0
 
-        MOV DH, 3
-        MOV DL, 30
-        MOV byte[200h], 5
-        MOV byte[201h], 7
-        MOV byte[202h], 9
+        MOV DL, 25
+
+        MOV byte[200h], 8
+        MOV byte[201h], 10
+        MOV byte[202h], 12
+        MOV byte[203h], 14
 
         call modotexto
 
         itNameOne:
-                call movercursor 
-                MOV CL, [cadenaName+SI] 
+                MOV DH, [200h] 
+                call movercursor
+                MOV CL, [cadenaName +SI]
                 call escribircaracter
                 XOR CL, CL
-                INC DL 
-                INC SI 
-                INC DI
-                CMP DI, 6; firstname: "Alexis"
+                INC DL
+                INC SI
+                CMP SI, 7; firstname: "Alexis"
                 JB itNameOne
-                call CmpEspacio
+                call spaceBlock
                 jmp itNameTwo
 
         itNameTwo:
-                MOV DH, [200h]
+                MOV DH, [201h]
                 call movercursor 
                 MOV CL, [cadenaName+SI] 
                 call escribircaracter
                 INC DL 
-                INC SI 
-                INC DI
-                CMP DI,6; second name: "Rafael"
+                INC SI
+                CMP SI,14; second name: "Rafael"
                 JB itNameTwo
-                call CmpEspacio
+                call spaceBlock
                 jmp itLastOne
 
         itLastOne:
-                MOV DH, [201h]
-                call movercursor 
-                MOV CL, [cadenaName+SI]
-                call escribircaracter
-                INC DL 
-                INC SI 
-                INC DI
-                CMP DI,7; first Lastname:"Bolaños"
-                JB itLastOne
-                call CmpEspacio
-                jmp itLastSecond
-        
-        itLastSecond:
                 MOV DH, [202h]
                 call movercursor 
                 MOV CL, [cadenaName+SI]
                 call escribircaracter
                 INC DL 
-                INC SI 
-                INC DI
-                CMP DI,5; second lastname: "Mejía"
+                INC SI
+                CMP SI,22; first Lastname:"Bolaños"
+                JB itLastOne
+                call spaceBlock
+                jmp itLastSecond
+        
+        itLastSecond:
+                MOV DH, [203h]
+                call movercursor 
+                MOV CL, [cadenaName+SI]
+                call escribircaracter
+                INC DL 
+                INC SI
+                CMP SI,27; second lastname: "Mejía"
                 JB itLastSecond
-                call CmpEspacio
+                call spaceBlock
                 jmp esperartecla
 
+        esperartecla:
+                MOV AH, 00h
+                INT 16h        
+
+        spaceBlock:
+                MOV DL, 25
+                RET
 
         modotexto: 
                 MOV AH, 0h
-                MOV AL, 00h
+                MOV AL, 00h; 25 filas, 40 columnas
                 INT 10h
                 RET
 
@@ -90,7 +90,9 @@ org 100h
                 MOV CX, 1h 
                 INT 10h
                 RET
-
+        
+        exit:
+                int 20h
 
         section .data
-        cadenaName DB 'Alexis Rafael Bolaños Mejía'
+        cadenaName DB 'Alexis Rafael Bolanos Mejia'
