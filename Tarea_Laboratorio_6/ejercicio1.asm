305ccc2fa2eec7764e9fe072b73707e6f4efffd5
unknown <00090718@uca.edu.sv>
2021-06-25 23:51:38 -0600

    Laboratorio 6: añadiendo logica de comparacion

--- Tarea_Laboratorio_6/ejercicio1.asm
+++ Tarea_Laboratorio_6/ejercicio1.asm
@@ -13,7 +13,7 @@ org 100h
 
         msg1 db "BIENVENIDO"
         msg2 db "INCORRECTO"
-        psw db "axis*"
+        pswd db "axis*"
         frase times 5 db " "
 
         ; FUNCIONES
@@ -24,3 +24,19 @@ org 100h
                 mov     AH, 01h         
                 int     21h
                 ret
+        
+        ;leerá la longitud de la cadena ingresada
+        leerCadena:
+                xor     SI, SI
+        while:
+                call EsperarTecla
+                cmp AL, 0x0D
+                je exit
+                MOV [BP+SI], AL
+                
+                ;logica de comparacion de la cadena y la clave ya quemada
+                MOV CL, [frase+SI] ;la cadena ingresada es alojada caracter por caracter
+                MOV BL, [pswd+SI] ;la pswd quemada es alojada caracter por caracter
+                INC SI            ;incrementamos el contador para realizar la comparacion entre cadenas
+                CMP BL, CL        ;comparación entre cadenas, específicamente caracter x caracter
+
