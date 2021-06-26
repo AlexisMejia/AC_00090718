d2aa86b18d32cd506818e7518388b6f0bacd04da
unknown <00090718@uca.edu.sv>
2021-06-26 02:57:27 -0600

    Laboratorio 6: fixing bugs

--- Tarea_Laboratorio_6/ejercicio1.asm
+++ Tarea_Laboratorio_6/ejercicio1.asm
@@ -1,19 +1,21 @@
 ;codigo base tecladoMS-DOS2
 org 100h
 
+        
         section .text
 
         ;ingresando la cadena a comparar
-        mov BP, cadena
-        call leerCadena
+        mov BP, frase
+        call LeerCadena
+        call compararCadena
         call EscribirCadena
         call EsperarTecla
 
         section .data
 
-        msg1 db "BIENVENIDO"
-        msg2 db "INCORRECTO"
-        pswd db "axis*"
+        msg1 db "BIENVENIDO", "$"
+        msg2 db "INCORRECTO", "$"
+        pswd db "axiss", "$"
         frase times 5 db " "
 
         ; FUNCIONES
@@ -25,18 +27,36 @@ org 100h
                 int     21h
                 ret
         
+
+        ; Permite escribir en la salida estándar una cadena de caracteres o string, este
+        ; debe tener como terminación el carácter “$”
+        ; Parámetros:	AH: 09h 	DX: dirección de la celda de memoria inicial de la cadena
+        EscribirCadena:
+                mov 	AH, 09h
+                int 	21h                             
+                ret
+        
+        ; Leer cadena de texto desde el teclado
+        ; Salida:       SI: longitud de la cadena 		BP: direccion de guardado
+        LeerCadena:
+                xor     SI, SI       ; SI = 0
+        condicion:  
+                call    EsperarTecla    ; retorna un caracter en AL
+                cmp     AL, 0x0D        ; comparar AL con caracter EnterKey
+                je      exit            ; si AL == EnterKey, saltar a exit
+                mov     [BP+SI], AL   	; guardar caracter en memoria
+                inc     SI              ; SI++
+                jmp     condicion           ; saltar a while
+
         ;leerá la longitud de la cadena ingresada
-        leerCadena:
+        compararCadena:
                 xor     SI, SI
-        while:
-                call EsperarTecla
-                cmp AL, 0x0D
-                je exit
-                MOV [BP+SI], AL
-                
-                ;logica de comparacion de la cadena y la clave ya quemada
-                MOV CL, [frase+SI] ;la cadena ingresada es alojada caracter por caracter
-                MOV BL, [pswd+SI] ;la pswd quemada es alojada caracter por caracter
+                xor     DI, DI
+        while:  
+                ;logica de comparacion de la cadena y la clave sean correctas
+                mov AL, [BP+SI]                ; Se guarda en AL lo que es te en BP+SI
+                cmp AL, [pswd+SI]  
                 INC SI            ;incrementamos el contador para realizar la comparacion entre cadenas
                 CMP BL, CL        ;comparación entre cadenas, específicamente caracter x caracter
+                JE banderaT       ; si el caracter es igual dará paso
 
