b5d79138b244080c4e096a54997e402db3e1b217
unknown <00090718@uca.edu.sv>
2021-06-27 02:56:20 -0600

    Laboratorio 6: done

--- Tarea_Laboratorio_6/ejercicio1.asm
+++ Tarea_Laboratorio_6/ejercicio1.asm
@@ -1,22 +1,71 @@
 ;codigo base tecladoMS-DOS2
 org 100h
-
-        
         section .text
 
-        ;ingresando la cadena a comparar
-        mov BP, frase
-        call LeerCadena
-        call compararCadena
+        mov DX, msg1
         call EscribirCadena
+
+        ;leer cadena ingresada
+        MOV BP, frase
+        call LeerCadena
+
         call EsperarTecla
+        int 20h
 
         section .data
-
-        msg1 db "BIENVENIDO", "$"
-        msg2 db "INCORRECTO", "$"
+        msg1 db	"", "$"		;Mensaje que pide la clave
+        msg2 db "BIENVENIDO", "$"
+        msg3 db "INCORRECTO", "$"
         pswd db "axiss", "$"
         frase times 5 db " "
+
+        ; Permite escribir en la salida estándar una cadena de caracteres o string, este
+        ; debe tener como terminación el carácter “$”
+        ; Parámetros:	AH: 09h 	DX: dirección de la celda de memoria inicial de la cadena
+        EscribirCadena:
+                mov 	AH, 09h
+                int 	21h                             
+                ret
+
+        ; Leer cadena de texto desde el teclado
+        ; Salida:       SI: longitud de la cadena 		BP: direccion de guardado
+        LeerCadena:
+                xor     SI, SI       ; SI = 0
+                xor     DI, DI
+        while:  
+                call    EsperarTecla    ; retorna un caracter en AL
+                cmp     AL, 0x0D        ; comparar AL con caracter EnterKey
+                je      cadena            ; si AL == EnterKey, saltar a exit
+                mov     [BP+SI], AL   	; guardar caracter en memoria
+                inc     SI              ; SI++
+                jmp     while           ; saltar a while
+
+        ;leerá la longitud de la cadena ingresada
+        cadena:
+                xor     SI, SI
+                ;xor     DI, DI
+        
+        for:
+                mov AL, [BP+SI]
+                cmp AL, [pswd+SI]
+                je banderaC
+                jne banderaF
+
+        banderaC:
+                cmp SI, 4
+                je mensajeC
+                inc SI
+                jmp for
+        
+        banderaF:
+                mov DX, msg3
+                call EscribirCadena
+                jmp exit
+
+        mensajeC:
+                MOV DX, msg2
+                call EscribirCadena
+                jmp exit
 
         ; FUNCIONES
         ; Permite leer un carácter de la entrada estándar con echo
@@ -26,37 +75,7 @@ org 100h
                 mov     AH, 01h         
                 int     21h
                 ret
-        
 
-        ; Permite escribir en la salida estándar una cadena de caracteres o string, este
-        ; debe tener como terminación el carácter “$”
-        ; Parámetros:	AH: 09h 	DX: dirección de la celda de memoria inicial de la cadena
-        EscribirCadena:
-                mov 	AH, 09h
-                int 	21h                             
+        exit:
+                mov byte [BP+SI], "$"
                 ret
-        
-        ; Leer cadena de texto desde el teclado
-        ; Salida:       SI: longitud de la cadena 		BP: direccion de guardado
-        LeerCadena:
-                xor     SI, SI       ; SI = 0
-        condicion:  
-                call    EsperarTecla    ; retorna un caracter en AL
-                cmp     AL, 0x0D        ; comparar AL con caracter EnterKey
-                je      exit            ; si AL == EnterKey, saltar a exit
-                mov     [BP+SI], AL   	; guardar caracter en memoria
-                inc     SI              ; SI++
-                jmp     condicion           ; saltar a while
-
-        ;leerá la longitud de la cadena ingresada
-        compararCadena:
-                xor     SI, SI
-                xor     DI, DI
-        while:  
-                ;logica de comparacion de la cadena y la clave sean correctas
-                mov AL, [BP+SI]                ; Se guarda en AL lo que es te en BP+SI
-                cmp AL, [pswd+SI]  
-                INC SI            ;incrementamos el contador para realizar la comparacion entre cadenas
-                CMP BL, CL        ;comparación entre cadenas, específicamente caracter x caracter
-                JE banderaT       ; si el caracter es igual dará paso
-
